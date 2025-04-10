94d20f77ae08d7870fc1c599a5d2fe978340bd4d
kimsuhanmuok <kimsuhanmuok@gmail.com>
2025-04-10 17:45:19 +0900

    py폴더에 있는 p70778690 파일 추가함

---
+++ p86.py
@@ -0,0 +1,9 @@
+print("# 하나만 출력합니다.")
+print("Hello Python Programing...!.")
+print()
+
+#여러 개를 출렵합니다
+print("# 여러 개를 출렵합니다.")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의","이름은", "윤인성 입니다!")
+print()
