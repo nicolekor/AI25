e831fd0687d0484e0e80a0eb6012e15057049f11
kimsuhanmuok <kimsuhanmuok@gmail.com>
2025-04-29 17:40:01 +0900

    250429 JAVA_eclipse 파일 업로드

---
+++ JAVA/ex_02/src/sec01/exam01/MyTest.java
@@ -0,0 +1,19 @@
+package sec01.exam01;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 0;
+		
+		for (int i=1; i <= 10; i++) {
+			sum += i;
+		}
+		// syso + ctrl + 스페이스  -> System.out.println()
+		System.out.println("1~100까지 합: " + sum);
+		
+	}
+	
+
+}
