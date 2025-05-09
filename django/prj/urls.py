40e3d8c8f8d0c743fa03ce5fa64e39e0cc5a1562
kimsuhanmuok <kimsuhanmuok@gmail.com>
2025-05-09 17:55:59 +0900

    250509 장고부트스트랩 p242까지 완료

--- django/prj/urls.py
+++ django/prj/urls.py
@@ -2,8 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')), # 기본 도메인 주소를 띄었을 때 나타나는 내용을 싱글페이지로 지정함
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
