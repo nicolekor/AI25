41c2263ced242100c4c219c62aea347d894550e5
kimsuhnamuok <kimsuhnamuok@email.com>
2025-05-13 17:49:56 +0900

    250513 django p295 까지

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -17,9 +18,10 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     # author : 추후 작성 예정
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
     
     def get_absolute_url(self):  #blog 게시글 상세 페이지에서 view on site 버튼 생성
         return f'/blog/{self.pk}/'
