af74ae1ad906af19f2c2fb381b8b4efbb5bc9c97
kimsuhanmuok <kimsuhanmuok@gmail.com>
2025-05-23 15:48:31 +0900

    250523 JSP 오전

---
+++ JSP_data/pro3/src/main/java/sec03/ex03/LoginServlet3.java
@@ -0,0 +1,42 @@
+package sec03.ex03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		String address = request.getParameter("user_address");
+		
+		System.out.println("아이디   : " + id); // 콘솔창에서 확인하려고 적음
+		System.out.println("비밀번호 : " + pw);  // 콘솔창에서 확인하려고 적음
+
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + id;
+		data += "<br>";
+		data += "비밀번호 : " + pw;
+		data += "<br>";
+		data += "주소 : " + address;
+		data += "</html>";
+		data += "</body>";
+		out.print(data);
+	
+	}
+
+}
