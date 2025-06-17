6b8a298290e866ebf0abd5160a9dbdbea3f545a2
kimsuhanmuok <kimsuhanmuok@gmail.com>
2025-06-17 17:17:13 +0900

    250617 부트3 교재 p110 까지

---
+++ spt_data/firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.dto;
+
+public class ArticleForm {
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+//    생성자 만드는 단축키 alt + insert
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
