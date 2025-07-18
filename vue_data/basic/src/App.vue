d0e57df57270d4c22f248b327d09e633bb80b5a4
kimsuhanmuok <kimsuhanmuok@gmail.com>
2025-07-18 17:25:54 +0900

    250718 vue.js & openCV

---
+++ vue_data/basic/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data () {
+    return {
+      condition: true,
+      
+    };
+  },
+}
+</script>
+<template>
+  <p v-show="condition"> 조건이 참이다.</p>
+  <p v-show="!condition">조건이 거짓이다.</p> 
+</template>
+<style></style>
